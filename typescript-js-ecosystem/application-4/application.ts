ce139555eef6f940147bf9835efb13a411a819eb
Sam Lanning <sam@samlanning.com>
2019-07-17 17:21:58 -0700

    Changes in preparation for talk

--- typescript-js-ecosystem/application-4/application.ts
+++ typescript-js-ecosystem/application-4/application.ts
@@ -4,6 +4,6 @@ const result1 = _.head([1, 2, 3, 4]);
 console.log(result1);
 // => 1
 
-const result2 = _.head([]);
+const result2 = _.haed([]);
 console.log(result2);
 // => undefined
--- typescript-js-ecosystem/application-5/application.ts
+++ typescript-js-ecosystem/application-5/application.ts
@@ -1,9 +1,16 @@
-import * as _ from 'lodash';
+import * as lightDesk from '@synesthesia-project/light-desk';
 
-const result1 = _.head([1, 2, 3, 4]);
-console.log(result1);
-// => 1
+function buttonPressed() {
+  console.log("The button was pressed!");
+}
 
-const result2 = _.head([]);
-console.log(result2);
-// => undefined
+const desk = new lightDesk.LightDesk();
+
+// Create the root "group" for your desk, which new components can be added to
+const group = new lightDesk.Group();
+desk.setRoot(group);
+
+// Create a button that calls buttonPressed(),
+const button = new lightDesk.Button("Hello World");
+group.addChild(button);
+button.addListener(buttonPressed);
