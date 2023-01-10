5c9ec9df890a840f10701c7e8db4ba88293e1583
Sam Lanning <samuel.lanning@transferwise.com>
2023-01-10 13:41:38 +0000

    step

--- migrating-large-projects/src/foo.ts
+++ migrating-large-projects/src/foo.ts
@@ -1,4 +1,5 @@
 import * as m1 from './some-library-module';
+import {someAggregateFunction} from './some-library-module.typed';
 
 console.log(m1.addTogether(12));
 console.log(m1.addTogether('foo'));
@@ -8,4 +9,4 @@ console.log(JSON.stringify(m1.toString([])));
 
 console.log(JSON.stringify(m1.toString(12)));
 
-console.log(m1.someAggregateFunction(12));
+console.log(someAggregateFunction(12));
