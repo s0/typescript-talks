bb56c3fffa8a85d9191980e9d84698f020938af3
Sam Lanning <samuel.lanning@transferwise.com>
2023-01-10 13:39:35 +0000

    step

--- migrating-large-projects/src/some-library-module.typed.ts
+++ migrating-large-projects/src/some-library-module.typed.ts
@@ -1,15 +1,15 @@
 import { makePair } from './another-library-module';
 
-export const someAggregateFunction = (value) => {
+export const someAggregateFunction = <T>(value: T) => {
   value = makePair(value);
   value = addLengthToArray(value);
   return getFirstAndLast(value);
 }
 
-const addLengthToArray = (value) => {
+const addLengthToArray = <T>(value: T[]): (T | number)[] => {
   return [...value, value.length];
 }
 
-const getFirstAndLast = (value) => {
+const getFirstAndLast = <T>(value: T[]): [T, T] => {
   return [value[0], value[value.length - 1]];
 }
